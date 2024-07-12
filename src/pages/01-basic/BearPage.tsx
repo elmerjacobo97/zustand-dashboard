52062f66f9f9c442cb5ad33f1955e780b96bc8dc
Elmer Jacobo Otiniano <88252912+elmerjacobo97@users.noreply.github.com>
2024-07-12 12:28:27 -0500

    fin seccion 2

--- src/pages/01-basic/BearPage.tsx
+++ src/pages/01-basic/BearPage.tsx
@@ -1,4 +1,6 @@
+import { useShallow } from 'zustand/react/shallow';
 import { WhiteCard } from '../../components';
+import { useBearStore } from '../../stores';
 
 export const BearPage = () => {
   return (
@@ -7,47 +9,86 @@ export const BearPage = () => {
       <p>Manejo de estado simple de Zustand</p>
       <hr />
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
+      <div className="grid grid-cols-1 gap-2 md:grid-cols-2 lg:grid-cols-3">
+        <BlackBears />
+        <PolarBears />
+        <PandaBears />
 
-        <WhiteCard centered>
-          <h2>Osos Negros</h2>
+        <BearsDisplay />
+      </div>
+    </>
+  );
+};
 
-          <div className="flex flex-col md:flex-row">
-            <button> +1</button>
-            <span className="text-3xl mx-2 lg:mx-10"> 0 </span>
-            <button>-1</button>
-          </div>
+const BlackBears = () => {
+  const blackBears = useBearStore((state) => state.blackBears);
+  const increaseBlackBears = useBearStore((state) => state.increaseBlackBears);
 
-        </WhiteCard>
+  return (
+    <WhiteCard centered>
+      <h2>Osos Negros</h2>
 
-        <WhiteCard centered>
-          <h2>Osos Polares</h2>
+      <div className="flex flex-col md:flex-row">
+        <button onClick={() => increaseBlackBears(+1)}>+1</button>
+        <span className="mx-2 text-3xl lg:mx-10"> {blackBears} </span>
+        <button onClick={() => increaseBlackBears(-1)}>-1</button>
+      </div>
+    </WhiteCard>
+  );
+};
 
-          <div className="flex flex-col md:flex-row">
-            <button> +1</button>
-            <span className="text-3xl mx-2 lg:mx-10"> 0 </span>
-            <button>-1</button>
+const PolarBears = () => {
+  const polarBears = useBearStore((state) => state.polarBears);
+  const increasePolarBears = useBearStore((state) => state.increasePolarBears);
 
-          </div>
+  return (
+    <WhiteCard centered>
+      <h2>Osos Polares</h2>
 
-        </WhiteCard>
+      <div className="flex flex-col md:flex-row">
+        <button onClick={() => increasePolarBears(+1)}> +1</button>
+        <span className="mx-2 text-3xl lg:mx-10"> {polarBears} </span>
+        <button onClick={() => increasePolarBears(-1)}>-1</button>
+      </div>
+    </WhiteCard>
+  );
+};
 
-        <WhiteCard centered>
-          <h2>Osos Pandas</h2>
+const PandaBears = () => {
+  const pandaBears = useBearStore((state) => state.pandaBears);
+  const increasePandaBears = useBearStore((state) => state.increasePandaBears);
 
-          <div className="flex flex-col md:flex-row">
-            <button> +1</button>
-            <span className="text-3xl mx-2 lg:mx-10"> 0 </span>
-            <button>-1</button>
-          </div>
+  return (
+    <WhiteCard centered>
+      <h2>Osos Pandas</h2>
+      <div className="flex flex-col md:flex-row">
+        <button onClick={() => increasePandaBears(+1)}> +1</button>
+        <span className="mx-2 text-3xl lg:mx-10"> {pandaBears} </span>
+        <button onClick={() => increasePandaBears(-1)}>-1</button>
+      </div>
+    </WhiteCard>
+  );
+};
 
-        </WhiteCard>
+export const BearsDisplay = () => {
+  // const bears = useBearStore((state) => state.bears); // <-- Sin usar useShallow (se renderizar todo el componente)
+  const bears = useBearStore(useShallow((state) => state.bears)); // <-- Usar useShallow (si el estado no cambia, no se renderizará el componente)
+  const doNothing = useBearStore((state) => state.doNothing);
 
+  const addBear = useBearStore((state) => state.addBear);
+  const clearBears = useBearStore((state) => state.clearBears);
 
+  return (
+    <WhiteCard>
+      <h1>Osos</h1>
 
-
+      <div className="flex flex-col gap-2">
+        <button onClick={doNothing}>Do Nothing</button>
+        <button onClick={addBear}>Add Bear</button>
+        <button onClick={clearBears}>Clear Bears</button>
       </div>
 
-    </>
+      <pre>{JSON.stringify(bears, null, 2)}</pre>
+    </WhiteCard>
   );
 };
